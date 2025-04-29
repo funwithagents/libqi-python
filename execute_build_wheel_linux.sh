b4107e2f0320724844e5f6382e856fae73f4baa0
thervier <thibault@fr2g.io>
2025-04-29 23:33:44 +0200

    Add scripts to setup and build on Linux

---
+++ execute_build_wheel_linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+conan profile detect
+
+# Set needed variables for libqi repository dependency
+QI_REPOSITORY="https://github.com/funwithagents/libqi.git"
+QI_VERSION="4.0.5"
+QI_PATH="$PWD/../libqi"
+rm -rf $QI_PATH
+# Clone liqi respository
+git clone --depth=1 --branch "qi-framework-v${QI_VERSION}" "${QI_REPOSITORY}" "${QI_PATH}"
+# Link the libqi dependency by loading its conanfile into conan cache 
+conan export "${QI_PATH}" --version "${QI_VERSION}" 
+
+# Build
+conan install . --build=missing -c tools.build:skip_test=true -c '&:tools.build:skip_test=true'
+cmake --list-presets # => it should list the preset "conan-linux-x86_64-gcc-release"
+cmake --preset conan-linux-x86_64-gcc-release
+cmake --build --preset conan-linux-x86_64-gcc-release -j8
+
+# Generate wheel
+python -m build --config-setting cmake.define.CMAKE_TOOLCHAIN_FILE=$PWD/build/linux-x86_64-gcc-release/generators/conan_toolchain.cmake
+
+# Repair wheel (using all the dependency libs that have been previously copied by conan)
+LD_LIBRARY_PATH=$PWD/build/linux-x86_64-gcc-release/lib/ auditwheel repair --strip --plat manylinux_2_34_x86_64 dist/qi-3.1.6-cp313-cp313-linux_x86_64.whl -w ./dist/fixed/
